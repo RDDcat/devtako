e4969e11b088c081fa7275aa18b7be24689f6cc0
RDDcat <yukmaro@gmail.com>
2021-08-10 10:29:59 +0900

    Initial commit

---
+++ src/main/java/com/tako/devtako/DevtakoApplication.java
@@ -0,0 +1,13 @@
+package com.tako.devtako;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtakoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevtakoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tako/devtako/DevtakoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tako.devtako;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevtakoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
