ce242c4576bcf316f23d87b79c0c1dba0be6e7bd
RDDcat <yukmaro@gmail.com>
2021-08-13 10:24:33 +0900

    [ADD] board api
    board api 추가

--- src/main/java/com/tako/devtako/repository/BoardRepository.java
+++ src/main/java/com/tako/devtako/repository/BoardRepository.java
@@ -4,5 +4,11 @@ import com.tako.devtako.domain.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    List<Board> findByTitle(String title);
+
+    List<Board> findByTitleOrContent(String title, String content);
 }
