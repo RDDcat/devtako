029616aa8d4cefa18573d521fa32ad2749205ac1
RDDcat <yukmaro@gmail.com>
2021-08-16 15:35:41 +0900

    [ADD] security config

--- src/main/java/com/tako/devtako/repository/BoardRepository.java
+++ src/main/java/com/tako/devtako/repository/BoardRepository.java
@@ -1,6 +1,8 @@
 package com.tako.devtako.repository;
 
 import com.tako.devtako.domain.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
@@ -11,4 +13,7 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     List<Board> findByTitle(String title);
 
     List<Board> findByTitleOrContent(String title, String content);
+
+    Page<Board> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
+
 }
